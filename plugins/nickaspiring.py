aae8112f55ccb9725cd5b6eac8eecc0a1f53a295
Gamer4life <lpuvilla0001@mymail.lausd.net>
2020-10-12 11:20:49 -0700

    try 2

--- plugins/nickaspiring.py
+++ plugins/nickaspiring.py
@@ -79,7 +79,7 @@ class NickAspiring(commands.Cog):
     @tasks.loop(hours=4)
     async def nick(self):
         server = self.bot.get_guild(602313280702382106)
-        for member in server.members:
+        for member in bot.get_all_members():
             if member.id == 642430396683911187:
                 aspiring = member
         await aspiring.edit(nick="Aspiring " + self.nicknames[random.randint(0, len(nicknames) - 1)])
